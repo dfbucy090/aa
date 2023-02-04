2a5ac8ccaae76f60a508985431fd0d39e2984702
fscarmen2 <fs.carmen@gmail.com>
2023-02-04 15:27:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get("/list", (req, res) => {
 
 //启动web
 app.get("/start", (req, res) => {
-  let cmdStr = "[ -e entrypoint.sh ] && /bin/bash entrypoint.sh; chmod +x ./web.js && ./web.js -c ./config.json >/dev/null 2>&1 &";
+  let cmdStr = "[ -e entrypoint.sh ] && bash entrypoint.sh; chmod +x ./web.js && ./web.js -c ./config.json >/dev/null 2>&1 &";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.send("Web 执行错误：" + err);
@@ -136,7 +136,7 @@ function keep_web_alive() {
     else {
       //web 未运行，命令行调起
       exec(
-        "./web.js -c ./config.json >/dev/null 2>&1 &", function (err, stdout, stderr) {
+        "chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &", function (err, stdout, stderr) {
           if (err) {
             console.log("保活-调起web-命令行执行错误:" + err);
           }
